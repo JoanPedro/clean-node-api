5c45b83e0643952f4fced8d027e7675b7935ce4c
Joan Pedro <jpos.senai@gmail.com>
2020-08-17 16:54:45 -0300

    refactor: turns to async methods

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -8,7 +8,7 @@ export class SignUpController implements Controller {
     private readonly addAccout: AddAccount
   ) {}
 
-  handle = (httpRequest: HttpRequest): HttpResponse => {
+  handle = async (httpRequest: HttpRequest): Promise<HttpResponse> => {
     try {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
@@ -24,7 +24,7 @@ export class SignUpController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
-      const account = this.addAccout.add({
+      const account = await this.addAccout.add({
         name,
         email,
         password
