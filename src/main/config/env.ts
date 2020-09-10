7f11a9be7e50e3c87d2dde2c5a73eba04ba5e280
Joan Pedro <jpos.senai@gmail.com>
2020-09-10 09:29:30 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'rj321#241Aionas#@*%asnd*23N324$13kH3#4&'
 }
